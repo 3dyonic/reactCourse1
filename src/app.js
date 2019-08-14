098294b26b7fa3f30c0a0e9a7aa353d49f8be635
Yonatan Lev Ari <yonatanl@codeoasis.com>
2019-08-14 18:54:45 +0300

    chapter 19

--- src/app.js
+++ src/app.js
@@ -9,53 +9,61 @@
 // only render the subtitle (and p tag) if subtitle exist -logical and operator
 // render a new p tag if options.length >0 "here are your options" "no options"
 
-const app = {
-    title: 'Indecision app',
-    subtitle: 'Put your life in the hands of a computer',
-    options: ['1', '2']
-}
-
-const template =
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{app.options.length > 0 ? 'here are your options' : 'no options'}</p>
-        <ol>
-            <li>item 1</li>
-            <li>item 2</li>
-        </ol>
-
-    </div>;
-
-let count = 0;
-const addOne = () => {
-    count++;
-    renderCounterApp();
-}
-const minusOne = () => {
-    if (count > 0) {
-        count--;
-        renderCounterApp()
-    }
-}
-
-const reset = () => {
-    count = 0;
-    renderCounterApp();
-}
 
 const appRoot = document.getElementById('app');
 
-const renderCounterApp = () => {
-    const templateTwo = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            {count > 0 && <button onClick={minusOne}>-1</button>}
-            {count > 0 && <button onClick={reset}>RESET</button>}
-        </div>
-    );
-    ReactDOM.render(templateTwo, appRoot);
+const app = {
+    title: 'Indecision app',
+    subtitle: 'Put your life in the hands of a computer',
+    options: []
 }
 
-renderCounterApp();
+const numbers = [55, 101, 1000];
+
+const onFormSubmit = (e) => {
+    e.preventDefault();
+    const option = e.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        renderIndicisionApp();
+    }
+    console.log('form submitted', app.options);
+}
+
+const resetOptions = () => {
+    app.options = [];
+    renderIndicisionApp();
+}
+
+
+
+const renderIndicisionApp = () => {
+    const template =
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'here are your options' : 'no options'}</p>
+            {app.options.length > 0 &&
+                <button onClick={resetOptions}>Remove all</button>
+            }
+            {
+                app.options.length > 0 &&
+                <ol>
+                    {
+                        app.options.map((option, index) => {
+                            return <li data-key={index} key={index}>option:{option}</li>
+                        })}
+                </ol>
+            }
+
+
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option" />
+                <button>add option</button>
+            </form>
+        </div>;
+    ReactDOM.render(template, appRoot);
+}
+
+renderIndicisionApp();
